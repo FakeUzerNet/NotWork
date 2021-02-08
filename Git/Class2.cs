b0b953d105989a3f6d5a99f7cd8abefc7b3396d5
User1 <isip_k.a.espenkov@mpt.ru>
2021-02-08 09:01:15 +0300

    add all

---
+++ Git/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git
+{
+    class Class2
+    {
+        static void nn()
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
+        }
+    }
+}
